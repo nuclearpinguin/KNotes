c580883dbba5f6304977d0f089764f81fc7307f7
nuclearpinguin <turbaszek@gmail.com>
2019-04-03 20:08:08 +0200

    feat (knotes) : get and remove books

--- knotes/src/Main.elm
+++ knotes/src/Main.elm
@@ -1,65 +1,216 @@
 module Main             exposing ( .. )
 
 import Browser
-import Html             exposing  ( Html, Attribute, button
-                                  , div, text, ul, li, a, h3, nav, h4, p, span, h2)
+import Html             exposing  ( Html, Attribute )
 import Html.Attributes  exposing  ( style, attribute, hidden, href)
 import Html.Events      exposing  ( onClick )
 import List             exposing  ( .. )
+import List.Extra       exposing ( uniqueBy )
+import Url.Builder      as Builder
+import Json.Encode      as Encode
+import Json.Decode      exposing (Decoder, decodeString, field, string, list, int, map2, map4 )
+import Http             exposing ( Error )
+import Debug            exposing ( log, toString )
 
-
-
-import Style            
-
+import Style
 
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+  Browser.element 
+    { init = init
+    , update = update
+    , view = view
+    , subscriptions = subscriptions
+    }
 
 -- MODEL
+type alias Book = 
+  { author : String
+  , title : String}
 
-type Msg = Smth | Other
+type alias Note = 
+  { book: Book
+  , body: String
+  , info: String
+  , id: Int
+  }
 
-type alias Model = Int
+type alias Load = 
+  { notes: List Note
+  , selectedNotes: List Note
+  , books: List Book
+  }
 
--- init : Game
--- init = 
---     { board = emptyBoard
---     , player = O
---     , state = NewGame
---     , gameType = Nil
---     }
+type Model = New 
+  | Loading 
+  | Failure Error 
+  | Loaded Load
+
+type Msg = Nil 
+  | GotNotes (Result Http.Error (List Note)) 
+  | Select Book
+  | LoadNotes
+  | RemoveNote Int
+  | Removed (Result Http.Error ())
+
+
+apiUrl : String
+apiUrl = "http://localhost:3000"
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( New
+  , Cmd.none
+  )
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+-- HTTP
+
+bookDecoder : Decoder Book
+bookDecoder = 
+  map2 Book  
+    (field "author" string)
+    (field "title" string)
+
+
+noteDecoder : Decoder Note
+noteDecoder = 
+  map4 Note
+    (field "book" bookDecoder)
+    (field "body" string)
+    (field "info" string)
+    (field "id" int)
+
+
+getNotes : Cmd Msg
+getNotes =
+  Http.get
+    { url = Builder.crossOrigin apiUrl ["notes"] []
+    , expect = Http.expectJson GotNotes (list noteDecoder)
+    }
+
+deleteNote : Int -> Cmd Msg
+deleteNote id =
+  Http.request
+    { method = "DELETE"
+    , headers = []
+    , url = Builder.crossOrigin apiUrl ["notes"] []
+    , body = id |> Encode.int |> Http.jsonBody
+    , expect = Http.expectWhatever Removed
+    , timeout = Nothing
+    , tracker = Nothing
+    }
 
 -- UPDATE
 
 
-update : Msg -> Model -> Model
-update msg model = model + 1
+filterBooks : Book -> List Note -> List Note
+filterBooks bk nts = filter (\n -> n.book == bk) nts
 
-makeNote : Int -> Html Msg
-makeNote n = 
-  div [style "padding-bottom" "20px", style "text-align" "left"] 
-    [ h4 [] [text "Title of the book"]
-    , span [style "color" "#C0C0C0"] [text (String.join " | " ["Name Author Jr.", "Some info about the note"]) ]
-    , p [] [text """ Read about The Elm Architecture to learn how to use this. Just do it. 
-            The additional context is very worthwhile! (Honestly, it is best to just 
-            read that guide from front to back instead of muddling around and reading it piecemeal.)"""]
-    ]
 
-makeBook : Int -> Html Msg
-makeBook n = li [style "list-style" "none"] [ button ( Style.btnText ++ [onClick Smth ]) [text "Book 1"]]
+makeNotes : List Note -> Model
+makeNotes notes =
+  let
+      bks = notes |> map (\n -> n.book) |> uniqueBy (\b -> b.title)
+      initNts = initNotes bks notes
+  in
+    Loaded {notes = notes, selectedNotes = initNts, books = bks}
 
+
+initNotes : List Book -> List Note -> List Note
+initNotes bks nts = 
+  case List.head bks of
+    Nothing -> []
+    Just bk -> filterBooks bk nts
+
+
+filterRemovedNote : Load -> Int -> Load
+filterRemovedNote load id = 
+    {load | 
+    notes = filter (\x -> (x.id /= id)) (load.notes), 
+    selectedNotes = filter (\x -> (x.id /= id)) (load.selectedNotes)
+    }
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  let
+      tmp = (msg, model)
+  in
+  case tmp of
+    (RemoveNote id, Loaded load )-> (Loaded (filterRemovedNote load id), deleteNote id)
+    (RemoveNote id, _ )-> (model, deleteNote id)
+    (LoadNotes, _ ) -> (model, getNotes)
+    (Select bk, Loaded load) -> (Loaded {load | selectedNotes = filterBooks bk load.notes}, Cmd.none)
+    (GotNotes result, _) ->
+      case result of
+        Ok notes ->  (makeNotes notes, Cmd.none)
+        Err err -> (Failure err, Cmd.none)
+    (_ , _) -> (model, Cmd.none)
 
 -- VIEW
 
+makeNoteView : Note -> Html Msg
+makeNoteView note = 
+  Html.div Style.noteDiv 
+    [ Html.h4 [] [Html.text note.book.title]
+    , Html.span [style "color" "#C0C0C0"] [Html.text (String.join " | " [note.book.author, note.info]) ]
+    , Html.p [] [Html.text note.body]
+    , Html.button [onClick (RemoveNote note.id)] [Html.text "Remove"]
+    ]
+
+
+makeBookView : Book -> Html Msg
+makeBookView book = 
+  Html.li 
+    [style "list-style" "none"] 
+    [ Html.button ( Style.btnText ++ [onClick (Select book) ]) [Html.text book.title ]]
+
+
+mainView : List (Html Msg) -> Html Msg
+mainView v = 
+    Html.div Style.mainDivLight
+    [ Html.nav Style.navBar [Html.h1 Style.navBarLogo [Html.text "KNotes"]]
+    , Html.div [style "margin-top" "50px"] v
+    ]
+
+
+loadedView : Load -> List (Html Msg)
+loadedView load = 
+  [ Html.div Style.leftMenu [ Html.ul [] (map makeBookView load.books) ]
+  , Html.div Style.notesList (map makeNoteView load.selectedNotes)
+  ]
+
+
+emptyView : List (Html Msg)
+emptyView =   
+  [ Html.div Style.leftMenu [ Html.ul [] [Html.text "No books yet."] ] 
+  , Html.div Style.notesList 
+    [ Html.text "Add some books!"
+    , Html.button [onClick LoadNotes] [Html.text "Load books"]
+    ]
+  ]
+
+
+errorView : Error -> List (Html Msg)
+errorView code =   
+  [ Html.div Style.leftMenu [ Html.ul [] [Html.text "No books yet."] ] 
+  , Html.div Style.notesList [ Html.text ("Whoops! Something went wrong!" ++ (toString code))]
+  ]
+
+
 view : Model -> Html Msg
 view model =
-  div [style "height" "100%", style "width" "100%", style "font-family" "arial", style "overflow" "hidden", style "position" "relative"]
-    [ nav [style "position" "fixed", style "width" "100%"] [h2 [style "margin-left" "40px"] [text "KNotes"]]
-    , div [style "margin-top" "50px"] 
-        [ div [ style "width" "15vw", style "height" "100%", style "position" "fixed"
-        , style "border" "10px", style "border-style" "none solid none none", style "border-color" "#C0C0C0", style "overflow-y" "hidden", style "overflow" "scroll"]
-            [ ul [] ([ h3 [] [text "Books"] ] ++ (map makeBook (range 0 100))) ]
-        , div [style "float" "right", style "margin-right" "5vw", style "width" "70vw", style "height" "100%"] 
-          ([text (String.fromInt model)] ++ map makeNote (range 0 100))
-        ]
-    ]
+  case model of
+  New -> emptyView |> mainView
+  Loading -> emptyView |> mainView
+  Failure code -> code |> errorView |> mainView 
+  Loaded load-> load |> loadedView |> mainView 
+
--- knotes/src/Style.elm
+++ knotes/src/Style.elm
@@ -1,16 +1,83 @@
 module Style            exposing (..)
-
 import Html             exposing (Attribute)
 import Html.Attributes  exposing (style)
 
-    
+
+makeStyle : List (String, String) -> List (Attribute a)
+makeStyle xs = List.map (\(x, y) -> style x y) xs
+
+
+mainDivLight : List (Attribute a)
+mainDivLight = makeStyle 
+    [ ("height", "100vh")
+    , ("width", "100vw")
+    , ("font-family", "arial")
+    , ("overflow", "hidden")
+    , ("position", "absolute")]
+
+
+mainDivDark : List (Attribute a)
+mainDivDark = makeStyle 
+    [ ("height", "100%")
+    , ("width", "100%")
+    , ("font-family", "arial")
+    , ("overflow", "hidden")
+    , ("position", "relative")
+    , ("background-color", "#1e1e1e")
+    , ("color", "#9f9f9f")]
+
+
+navBar : List (Attribute a)
+navBar = makeStyle
+    [ ("position", "fixed")
+    , ("width", "100%")
+    , ("height", "4vh")
+    , ("overflow", "visible")] 
+
+
+navBarLogo : List (Attribute a)
+navBarLogo = makeStyle
+    [ ("font-family", "arial")
+    , ("font-size", "24pt")
+    , ("margin-left", "40px")
+    , ("margin-top", "4px")]
+
+
+leftMenu : List (Attribute a)
+leftMenu = makeStyle
+    [ ("width", "15vw")
+    , ("height", "100%")
+    , ("position", "fixed")    
+    , ("border", "4px")
+    , ("border-style", "none solid none none")
+    , ("border-color", "#C0C0C0")
+    , ("overflow-y", "hidden")
+    , ("overflow", "auto") ]
+
+
 btnText : List (Attribute a) 
-btnText = 
-    [  style "border" "none"
-    , style "background-color" "inherit"
-    , style "padding" "10px"
-    , style "font-size" "12pt"
-    , style "cursor" "pointer"
-    , style "display" "inline-block"
+btnText = makeStyle  
+    [ ("border", "none")
+    , ("background-color", "inherit")
+    , ("padding", "10px")
+    , ("font-size", "12pt")
+    , ("cursor", "pointer")
+    , ("width", "100%")
+    -- , ("display", "inline-block")
+    , ("text-align", "left")
     ]
+
+
+notesList : List (Attribute a)
+notesList = makeStyle
+    [ ("float", "right")
+    , ("margin-right", "5vw")
+    , ("width", "70vw")
+    , ("height", "100%")]
+
+
+noteDiv : List (Attribute a)
+noteDiv = makeStyle  
+    [ ("padding-bottom", "20px")
+    , ("text-align", "left") ]
     
